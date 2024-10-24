7b3dd205aad4838125a9f7b8dae8fce7f3f8e5db
webtesting22 <webtesting2232@gmail.com>
2024-10-24 22:56:20 +0530

    Updated

--- src/Components/UploadVideos.jsx
+++ src/Components/UploadVideos.jsx
@@ -3,6 +3,14 @@ import { Row, Col } from "antd";
 
 import Delivery from "./Videos/Delivery Ads.mp4"
 import Educational from "./Videos/Educational Niche.mp4"
+import Portfolio from "./Videos/Portfolio  show reel.mp4"
+import sample6 from "./Videos/sample6.mp4"
+import sample9 from "./Videos/sample9.mp4"
+import sample10 from "./Videos/sample10.mp4"
+import ShortForm from "./Videos/Short Form.mp4"
+import Video1 from "./Videos/Video1.mp4"
+import Video2 from "./Videos/Video2.mp4"
+import Video3 from "./Videos/Video3.mp4"
 const UploadVideos = () => {
     const UploadVideoData = [
         {
@@ -10,7 +18,32 @@ const UploadVideos = () => {
         },
         {
             video:Educational
-        }
+        },
+        {
+            video:Portfolio
+        },
+        {
+            video:sample6
+        },
+        {
+            video:sample9
+        },
+        {
+            video:sample10
+        },
+        {
+            video:ShortForm
+        },
+        {
+            video:Video1
+        },
+        {
+            video:Video2
+        },
+        {
+            video:Video3
+        },
+
     ]
     return (
         <>
