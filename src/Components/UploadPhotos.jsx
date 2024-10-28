ea398dd861c91a7a3eab06694a8314f63051d6b9
Vinayak pandey <vrpandey2003@gmail.com>
2024-10-28 14:01:34 +0530

    mittal product

--- src/Components/UploadPhotos.jsx
+++ src/Components/UploadPhotos.jsx
@@ -16,6 +16,19 @@ import Channels1 from "./Images/MittalGroup/Channels1.jpeg"
 import ChannelsBar1 from "./Images/MittalGroup/ChannelsBar1.jpeg"
 import FlatBars1 from "./Images/MittalGroup/FlatBars1.jpeg"
 import RoundBars1 from "./Images/MittalGroup/RoundBars1.jpeg"
+import FlatBars2 from "./Images/MittalGroup/flatbar2.jpg"
+import FlatBars3 from "./Images/MittalGroup/flatbar3.jpg"
+import FlatBars4 from "./Images/MittalGroup/flatbar4.jpg"
+import FlatBars5 from "./Images/MittalGroup/flatbar5.jpg"
+import FlatBars6 from "./Images/MittalGroup/flatbar6.jpg"
+import Angles2 from "./Images/MittalGroup/angles2.jpg"
+import Angles3 from "./Images/MittalGroup/angles3.jpg"
+import Angles4 from "./Images/MittalGroup/angles4.jpg"
+import Angles5 from "./Images/MittalGroup/angles5.jpg"
+import Channels2 from "./Images/MittalGroup/channels2.jpg"
+import Channels3 from "./Images/MittalGroup/channels3.jpg"
+import Channels4 from "./Images/MittalGroup/channels4.jpg"
+import Channels5 from "./Images/MittalGroup/channels5.jpg"
 
 // ----------------GlosemImages-------------------
 import Bhavikpatel from "./Images/GlosemImages/BhavikPatel.jpeg"
@@ -27,6 +40,9 @@ import Health from "./Images/GlosemImages/healthcare.jpeg"
 import Robotics from "./Images/GlosemImages/robotics.jpeg"
 import Consumer from "./Images/GlosemImages/consumer.jpeg"
 import Communication from "./Images/GlosemImages/communication.jpeg"
+
+
+
 const UploadPhotos = () => {
 
     const UploadPhotoData = [
@@ -105,6 +121,45 @@ const UploadPhotos = () => {
         },
         {
             img:Communication,
+        },
+        {
+            img:FlatBars2
+        },
+        {
+            img:FlatBars3
+        },
+        {
+            img:FlatBars4
+        },
+        {
+            img:FlatBars5
+        },
+        {
+            img:FlatBars6
+        },
+        {
+            img: Angles2
+        },
+        {
+            img: Angles3
+        },
+        {
+            img: Angles4
+        },
+        {
+            img: Angles5
+        },
+        {
+            img:Channels2
+        },
+        {
+            img:Channels3
+        },
+        {
+            img:Channels4
+        },
+        {
+            img:Channels5
         }
 
     ]
